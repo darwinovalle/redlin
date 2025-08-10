06621de6a240f0ee6d090d67fa614619c7b106c9
davidovalle <dsovallep@unal.edu.co>
2025-08-10 15:05:28 -0500

    Add swagger api doc

--- redlin_web/redlin_web/settings.py
+++ redlin_web/redlin_web/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'google.generativeai',
     'dotenv',
     'corsheaders',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
 ]
 
 MIDDLEWARE = [
@@ -149,3 +151,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF & Schema
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Redlin API',
+    'DESCRIPTION': 'Documentación de la API del backend de Redlin.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
