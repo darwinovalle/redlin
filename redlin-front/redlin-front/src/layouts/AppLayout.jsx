75d3a55d53986f18b9c08f4b5729189471918218
darwinovalle <dovallep@unal.edu.co>
2025-08-25 18:19:56 -0500

    Rearrenge the sidebar style

--- redlin-front/redlin-front/src/layouts/AppLayout.jsx
+++ redlin-front/redlin-front/src/layouts/AppLayout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
   const navigate = useNavigate();
 
   return (
-    <Box sx={{ position: 'relative', display: 'flex', height: '100vh', bgcolor: 'transparent' }}>
+  <Box sx={{ position: 'relative', display: 'flex', height: '100vh', bgcolor: 'transparent', zIndex: 5 }}>
       <WavyBackground waveHeight="60vh" offsetY={0} />
       <MiniDrawer onLogout={() => { logout(); navigate('/login'); }} />
       <Box sx={{ position: 'relative', zIndex: 1, flexGrow: 1, height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'flex-start', overflowY: 'auto', pt: 0, mt: 0 }}>
